a6f33cb98aa185afc2b4c3f27f5f6cb3b036a245
puneet pandey <33287005+ppuneet64@users.noreply.github.com>
2020-09-16 21:09:35 +0530

    Create Regex.js
    Added new regular expression for email validation

---
+++ Regex.js
@@ -0,0 +1,2 @@
+const EMAIL_REGEX = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/i
+const EMAIL_MOBILE_REGEX = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})|([0-9]{10})+$/i
