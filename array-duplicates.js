c0119e754f5de5d4f453a5c8f7731edfa1f88c25
puneet pandey <33287005+ppuneet64@users.noreply.github.com>
2021-07-13 11:18:00 +0530

    Update array-duplicates.js
    added functions for duplicate

--- array-duplicates.js
+++ array-duplicates.js
@@ -1,15 +1,29 @@
-const getDuplicates = (arr) => {
+const getDuplicates1 = (arr) => {
     let duplicates = []
     arr.sort()
     for (let i = 0; i < arr.length - 1; i++) {
-        if (arr[i] === arr[i + 1]) {
+        if (arr[i] === arr[i + 1] && !duplicates.includes(arr[i])) {
             duplicates.push(arr[i])
         }
     }
     return duplicates
 }
 
-const getDuplicatesStrings = (arr) => {
+const getDuplicates2 = (arr) => {
+    let duplicates = []
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] === arr[j] && !duplicates.includes(arr[i])) {
+                duplicates.push(arr[j])
+            }
+        }
+    }
+    return duplicates
+}
+
+const getDuplicates3 = (arr) => arr.filter((el, index) => arr.indexOf(el) !== index)
+
+const getDuplicates4 = (arr) => {
     let duplicates = []
     let store = new Set()
     for (let i = 0; i < arr.length; i++) {
